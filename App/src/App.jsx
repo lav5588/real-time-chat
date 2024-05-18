376058a8436cccebbbde3a0fe2aaa186bf761ec6
LAV KUMAR YADAV <lavkumaryadavanshu@gmail.com>
2024-05-18 22:12:43 +0530

    some modifiction

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -9,7 +9,7 @@ import { addUsers, receivedChat, removeUser, setInitialUsers, setMySocketData, s
 
 
 
-export const socket=io.connect("https://real-time-chat-phi.vercel.app", { transports: ['websocket'] })
+export const socket=io.connect("http://localhost:3500", { transports: ['websocket'] })
 const App = () => {
   const dispatch = useDispatch();
   const selectedChatindex=useSelector(state=>state.chatReducer.selectedChatindex);
